10e0eeae2f0f68189b5662b4500d105ae4b72f9f
Stefan Uebe <s.uebe@proventis.net>
2018-02-15 13:55:30 +0100

    method to recalculate contents based on serving size

--- src/main/java/net/aza/recipes/view/details/IngredientsPart.java
+++ src/main/java/net/aza/recipes/view/details/IngredientsPart.java
@@ -1,23 +1,35 @@
 package net.aza.recipes.view.details;
 
-import java.util.List;
-
 import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
-
 import net.aza.recipes.model.Ingredient;
 
-public class IngredientsPart extends VerticalLayout {
-	public IngredientsPart(final int servingSize, final List<Ingredient> list) {
+import java.util.Collection;
+
+/**
+ * Displays a list of ingredients. The amount of ingredients shown is calculated based on the given serving size.
+ * This means that the ingredients original amount must be based on a serving size of 1.
+ */
+class IngredientsPart extends VerticalLayout {
+	private Collection<Ingredient> ingredients;
+
+	IngredientsPart(final int servingSize, final Collection<Ingredient> list) {
+		ingredients = list;
 		// TODO extract ingredients to separated, updateable components.
 
 		setDefaultComponentAlignment(Alignment.TOP_CENTER);
 
-		list.forEach(i -> {
-			addComponent(new Label("<b>" + i.getAmount() + "</b> " + i.getUnit(), ContentMode.HTML));
-		});
-
+		updateIngredients(servingSize);
 	}
+
+	public void updateIngredients(int servingSize) {
+		removeAllComponents();
+		ingredients.forEach(i -> {
+			addComponent(new Label("<b>" + (i.getAmount() * servingSize) + "</b> " + i.getUnit(), ContentMode.HTML));
+		});
+	}
+
+
 }
